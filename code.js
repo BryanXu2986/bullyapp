ef450c3dded29d0b522bfb089e7e7b99ca0b6f1b
BryanXu2986 <bryanxu2986@gmail.com>
2023-06-15 20:43:09 -0400

    first commit

---
+++ code.js
@@ -0,0 +1,96 @@
+onEvent("info1", "click", function( ) {
+  infoButton();
+});
+onEvent("info2", "click", function( ) {
+  infoButton();
+});
+onEvent("info3", "click", function( ) {
+  infoButton();
+});
+onEvent("info4", "click", function( ) {
+  infoButton();
+});
+onEvent("tips1", "click", function( ) {
+  tipsButton();
+});
+onEvent("tips2", "click", function( ) {
+  tipsButton();
+});
+onEvent("tips3", "click", function( ) {
+  tipsButton();
+});
+onEvent("tips4", "click", function( ) {
+  tipsButton();
+});
+onEvent("links1", "click", function( ) {
+  linksButton();
+});
+onEvent("links2", "click", function( ) {
+  linksButton();
+});
+onEvent("links3", "click", function( ) {
+  linksButton();
+});
+onEvent("links4", "click", function( ) {
+  linksButton();
+});
+onEvent("portfolio1", "click", function( ) {
+  portfolioButton();
+});
+onEvent("portfolio2", "click", function( ) {
+  portfolioButton();
+});
+onEvent("portfolio3", "click", function( ) {
+  portfolioButton();
+});
+onEvent("portfolio4", "click", function( ) {
+  portfolioButton();
+});
+onEvent("intro1", "click", function( ) {
+  introButton();
+});
+onEvent("intro2", "click", function( ) {
+  introButton();
+});
+onEvent("intro3", "click", function( ) {
+  introButton();
+});
+onEvent("intro4", "click", function( ) {
+  introButton();
+});
+onEvent("site1", "click", function( ) {
+  open("https://www.bullyingcanada.ca/");
+});
+onEvent("site2", "click", function( ) {
+  open("https://kidshealth.org/en/teens/bullies.html");
+});
+onEvent("site3", "click", function( ) {
+  open("https://keltymentalhealth.ca/bullying");
+});
+onEvent("image4", "click", function( ) {
+  open("https://www.bullyingcanada.ca/");
+});
+onEvent("image5", "click", function( ) {
+  open("https://kidshealth.org/en/teens/bullies.html");
+});
+onEvent("image6", "click", function( ) {
+  open("https://keltymentalhealth.ca/bullying");
+});
+onEvent("label11", "click", function( ) {
+  open("https://portfolio-assignment--bryanxu8.repl.co/index.html");
+});
+function infoButton() {
+  setScreen("information");
+}
+function tipsButton() {
+  setScreen("tips");
+}
+function linksButton() {
+  setScreen("links");
+}
+function introButton() {
+  setScreen("intro");
+}
+function portfolioButton() {
+  setScreen("portfolio");
+}
